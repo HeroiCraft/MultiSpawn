877d9b01c03295d2c9e0621ac5229a42372587c3
HeroCC <herocc@herocc.com>
2020-04-08 22:16:09 -0400

    Fix an issue where players can use use /spawn regardless of permission

--- src/main/java/com/herocc/bukkit/multispawn/commands/SpawnCommand.java
+++ src/main/java/com/herocc/bukkit/multispawn/commands/SpawnCommand.java
@@ -13,34 +13,42 @@ public class SpawnCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (plugin.getSpawnUtils().getNumberOfSpawns() == 0){ sender.sendMessage(ChatColor.RED + "There are no configured spawns!"); return true;} // Break if there are no spawns
-      if (args.length == 0 && sender instanceof Player){
-        // Sender must be player if none is specified
-        Player p = (Player) sender;
-        plugin.getSpawnUtils().sendPlayerToSpawn(p); // Send self to spawn
-        return true;
-      } else if (args.length == 1 && sender instanceof Player){ // Sender must be player
-        Player p = (Player) sender;
-        String spawn = args[0]; // Spawn is first arg
-        if (p.hasPermission("multispawn.bycommand." + spawn) || p.hasPermission("multispawn.spawn." + spawn)){
-          // If sender has permission to use command and specified spawn
-          plugin.getSpawnUtils().sendPlayerToSpawn(p, spawn);
-          return true;
-        }
-      } else if (args.length == 2){ // Assuming a spawn and player are specified
-        String spawn = args[0];
-        @SuppressWarnings("deprecation") // For some reason getting player by name is deprecated
-        Player p = plugin.getServer().getPlayer(args[1]);
-        if (p == null) { // If target is offline break
-          sender.sendMessage(ChatColor.RED + "Player " + args[1] + " isn't online!");
-          return true;
-        }
-        if ((sender.hasPermission("multispawn.bycommand" + spawn) || sender.hasPermission("multispawn.spawn." + spawn)) && sender.hasPermission("multispawn.others")){
-          // If sender has permission to use command, to TP others, and specific spawn
-          plugin.getSpawnUtils().sendPlayerToSpawn(p, spawn);
-          sender.sendMessage(p.getDisplayName() + ChatColor.GREEN + " sent to '" + spawn + "'");
-          return true;
-        }
+    if (args.length != 2 && !(sender instanceof Player)) { sender.sendMessage(ChatColor.RED + "Robots can't run that!"); return true; }
+
+    if (args.length == 0) {
+      // Sender must be player if none is specified
+      Player p = (Player) sender;
+      if (p.hasPermission("multispawn.bycommand")){
+        plugin.getSpawnUtils().sendPlayerToSpawn(p);
       } else {
+        p.sendMessage(ChatColor.RED + "You don't have permission to do that!");
+      }
+      return true;
+    } else if (args.length == 1) { // Sender must be player
+      Player p = (Player) sender;
+      String spawn = args[0]; // Spawn is first arg
+      if (p.hasPermission("multispawn.bycommand." + spawn)){
+        // If sender has permission to use command and specified spawn
+        plugin.getSpawnUtils().sendPlayerToSpawn(p, spawn);
+      } else {
+        p.sendMessage(ChatColor.RED + "You don't have permission for that spawn!");
+      }
+      return true;
+    } else if (args.length == 2) { // Assuming a spawn and player are specified
+      String spawn = args[0];
+      @SuppressWarnings("deprecation") // For some reason getting player by name is deprecated
+              Player p = plugin.getServer().getPlayer(args[1]);
+      if (p == null) { // If target is offline break
+        sender.sendMessage(ChatColor.RED + "Player " + args[1] + " isn't online!");
+        return true;
+      }
+      if (sender.hasPermission("multispawn.bycommand" + spawn) && sender.hasPermission("multispawn.others")){
+        // If sender has permission to use command, to TP others, and specific spawn
+        plugin.getSpawnUtils().sendPlayerToSpawn(p, spawn);
+        sender.sendMessage(p.getDisplayName() + ChatColor.GREEN + " sent to '" + spawn + "'");
+        return true;
+      }
+    } else {
       sender.sendMessage(ChatColor.RED + "Silly " + sender.getName() + ", you can't do that!");
       return true;
     }
