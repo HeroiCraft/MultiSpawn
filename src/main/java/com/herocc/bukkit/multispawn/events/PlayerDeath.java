66a9ee1cf2a686cef24ce7870914359c6f6a1e7c
HeroCC <herocc@herocc.com>
2020-04-09 01:35:51 -0400

    Remove conditions that will never happen
    GetSpawns always makes a new empty ArrayList, so it should never be null

--- src/main/java/com/herocc/bukkit/multispawn/events/PlayerDeath.java
+++ src/main/java/com/herocc/bukkit/multispawn/events/PlayerDeath.java
@@ -14,7 +14,6 @@ public class PlayerDeath implements Listener {
   public void onPlayerDeath(PlayerDeathEvent ev) {
     final Player p = ev.getEntity();
     if (p.hasPermission("multispawn.noteleport") // If player is excluded
-      || plugin.getSpawnUtils().getSpawns(p, false) == null // If spawns are null
       || plugin.getSpawnUtils().getSpawns(p, false).isEmpty()) return; // If spawns are empty
       
     if (plugin.getSpawnUtils().getSpawns(p, true).size() == 1
--- src/main/java/com/herocc/bukkit/multispawn/events/PlayerJoin.java
+++ src/main/java/com/herocc/bukkit/multispawn/events/PlayerJoin.java
@@ -16,7 +16,6 @@ public class PlayerJoin implements Listener {
   public void onPlayerJoin(PlayerJoinEvent ev) {
     final Player p = ev.getPlayer();
     if (p.hasPermission("multispawn.noteleport") // If player is excluded
-        || plugin.getSpawnUtils().getSpawns(p, false) == null // If spawns are null
         || plugin.getSpawnUtils().getSpawns(p, false).isEmpty()) return; // If spawns are empty
   
     if (plugin.getSpawnUtils().getSpawns(p, true).size() == 1
