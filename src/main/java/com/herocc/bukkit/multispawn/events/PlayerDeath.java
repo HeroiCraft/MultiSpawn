b5e338dc97030dcdc02299a11800816dabb104f7
Iaiao <err266228@gmail.com>
2021-01-07 21:18:01 +0200

    replace PlayerDeathEvent with PlayerRespawnEvent

--- src/main/java/com/herocc/bukkit/multispawn/events/PlayerDeath.java
+++ src/main/java/com/herocc/bukkit/multispawn/events/PlayerDeath.java
@@ -4,22 +4,24 @@ import com.herocc.bukkit.multispawn.MultiSpawn;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerDeath implements Listener {
   private final MultiSpawn plugin = MultiSpawn.getPlugin();
 
   @EventHandler
   @SuppressWarnings("unused")
-  public void onPlayerDeath(PlayerDeathEvent ev) {
-    final Player p = ev.getEntity();
-    if (p.hasPermission("multispawn.noteleport") // If player is excluded
+  public void onPlayerDeath(PlayerRespawnEvent ev) {
+    final Player p = ev.getPlayer();
+    if (p.hasPermission("multispawn.noteleportondeath") // If player is excluded
       || plugin.getSpawnUtils().getSpawns(p, false).isEmpty()) return; // If spawns are empty
       
     if (plugin.getSpawnUtils().getSpawns(p, true).size() == 1
       && plugin.getSpawnUtils().getSpawns(p, true).contains("default")
       && !plugin.getConfig().getBoolean("useDefaultAsFallback", true)) return;
-    
-    plugin.getSpawnUtils().sendPlayerToSpawn(p); // Teleport player if spawn list isn't empty
+
+    if (!plugin.getSpawnUtils().getSpawns(p, false).isEmpty()) {
+      ev.setRespawnLocation(plugin.getSpawnUtils().getSpawnLocation(plugin.getSpawnUtils().getRandomSpawn(p)));
+    }
   }
 }
